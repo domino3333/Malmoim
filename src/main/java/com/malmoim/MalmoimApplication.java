1294b4d1ab71e4c31b6b2ff25d2b45aff11821e6
domino3333 <busster@naver.com>
2026-06-02 08:08:26 +0900

    init

---
+++ src/main/java/com/malmoim/MalmoimApplication.java
@@ -0,0 +1,13 @@
+package com.malmoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalmoimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalmoimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malmoim/MalmoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malmoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalmoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
