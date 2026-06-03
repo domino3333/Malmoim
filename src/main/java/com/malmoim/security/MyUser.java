5b82bc9d90ae3c26e3fbf133ecf167f02db48ce6
domino3333 <busster@naver.com>
2026-06-03 22:44:41 +0900

    모든 멤버는 하나의 권한만 가지게 할 것이므로 SimpleGrantedAuthority는 굳이 리스트로 할 필요 없을 듯

--- src/main/java/com/malmoim/security/MyUser.java
+++ src/main/java/com/malmoim/security/MyUser.java
@@ -3,6 +3,7 @@ package com.malmoim.security;
 
 import lombok.Builder;
 import lombok.Data;
+import org.apache.ibatis.javassist.Loader;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,8 +24,9 @@ public class MyUser implements UserDetails {
     private String role;
     private LocalDateTime createdAt;
 
+    private SimpleGrantedAuthority authority;
+    //모든 유저는 권한이 하나뿐이라 리스트가 필요없지만 일단은 만들어놓음
     private List<SimpleGrantedAuthority> authorities;
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/com/malmoim/security/MyUserDetailsService.java
+++ src/main/java/com/malmoim/security/MyUserDetailsService.java
@@ -4,10 +4,14 @@ package com.malmoim.security;
 import com.malmoim.domain.Member;
 import com.malmoim.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +28,14 @@ public class MyUserDetailsService implements UserDetailsService {
             throw new RuntimeException("loadUserByUsername: member 없음");
         }
 
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(member.getRole());
 
-
-
-        return null;
+        return MyUser.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .name(member.getName())
+                .authority(authority)
+                .createdAt(member.getCreatedAt())
+                .build();
     }
 }
