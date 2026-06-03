dba9c9e0030ba0f9d578bb90951b80fd11259d08
domino3333 <busster@naver.com>
2026-06-03 22:13:38 +0900

    user타입 구현

--- src/main/java/com/malmoim/security/MyUser.java
+++ src/main/java/com/malmoim/security/MyUser.java
@@ -3,8 +3,61 @@ package com.malmoim.security;
 
 import lombok.Builder;
 import lombok.Data;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
-public class MyUser {
+public class MyUser implements UserDetails {
+
+    private Long no;
+    private String email;
+    private String password;
+    private String name;
+    private String role;
+    private LocalDateTime createdAt;
+
+    private List<SimpleGrantedAuthority> authorities;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
