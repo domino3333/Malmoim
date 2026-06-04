601a2bbb0fd1d18c2611781fdd4b2ae43f5e7531
domino3333 <busster@naver.com>
2026-06-04 21:30:47 +0900

    JwtAuthenticationFilter 작성

---
+++ src/main/java/com/malmoim/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.malmoim.security.jwt;
+
+
+import com.malmoim.security.MyUser;
+import com.malmoim.security.MyUserDetailsService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+/**
+ * 토큰을 들고 올 때 토큰을 검증하는 필터
+ * */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if(header!=null && header.startsWith("Bearer")){
+            String token = header.substring(7);
+
+            if(jwtTokenProvider.validateToken(token)){
+                String email = jwtTokenProvider.getEmail(token);
+                MyUser myUser = (MyUser) myUserDetailsService.loadUserByUsername(email);
+
+                UsernamePasswordAuthenticationToken authenticationToken
+                        = new UsernamePasswordAuthenticationToken(myUser,null,myUser.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+
+            }
+
+        }
+        filterChain.doFilter(request,response);
+
+
+    }
+}
