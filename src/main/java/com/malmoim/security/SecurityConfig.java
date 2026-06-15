ceafdede51f63192e6e5faaf0a4b3d2d4d1ed562
domino3333 <busster@naver.com>
2026-06-15 17:47:05 +0900

    웹소켓 연결 성공

--- src/main/java/com/malmoim/security/SecurityConfig.java
+++ src/main/java/com/malmoim/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth->auth
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/room/**").permitAll()
-                                .requestMatchers("/ws","/ws**").permitAll()
+                                .requestMatchers("/ws","/ws/**").permitAll()
 
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
