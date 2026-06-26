06caa852c8c880cdf667c0de1d8fb03bf672a4f0
domino3333 <busster@naver.com>
2026-06-27 02:02:41 +0900

    timerInfo 내려오는거 확인 완료

--- src/main/java/com/malmoim/dto/room/qna/timer/StartTimerResponse.java
+++ src/main/java/com/malmoim/dto/room/qna/timer/StartTimerResponse.java
@@ -1,12 +1,16 @@
 package com.malmoim.dto.room.qna.timer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StartTimerResponse {
     private Long roomNo;
     private String status;
