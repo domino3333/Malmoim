0c56e2aeed7b22377879c3e18819f26c56d89d17
domino3333 <busster@naver.com>
2026-07-21 23:52:33 +0900

    principal 필드 수정

--- src/main/java/com/malmoim/security/ParticipantPrincipal.java
+++ src/main/java/com/malmoim/security/ParticipantPrincipal.java
@@ -7,6 +7,7 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,7 +15,8 @@ import java.util.List;
 @Builder
 public class ParticipantPrincipal implements UserDetails {
 
-    
+    private Long roomNo;
+    private String nickname;
 
 
     @Override
