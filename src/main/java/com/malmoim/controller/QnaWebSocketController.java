9d60f0181ccac140e0478e4a46d3bf39115b8392
domino3333 <busster@naver.com>
2026-07-28 12:06:37 +0900

    웹소켓 컨트롤러에서 참여자 토큰의 인증 정보 받도록 수정

--- src/main/java/com/malmoim/controller/QnaWebSocketController.java
+++ src/main/java/com/malmoim/controller/QnaWebSocketController.java
@@ -2,12 +2,16 @@ package com.malmoim.controller;
 
 
 import com.malmoim.dto.room.qna.QnaQuestionMessage;
+import com.malmoim.security.ParticipantPrincipal;
 import com.malmoim.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,11 +24,19 @@ public class QnaWebSocketController {
 
     @MessageMapping("/qna/register")
     // 참여자가 보낸 질문을 같은 방의 모든 구독자에게 전달.
-    public void broadcastQuestion(QnaQuestionMessage dto) {
+    public void broadcastQuestion(QnaQuestionMessage dto, Principal principal) {
 
         //참여자가 보낸 질문 로그로 테스트
         log.info("websocket server dto:{} ", dto.getQuestion());
         log.info("websocket server dto:{} ", dto.getRoomNo());
+
+        Authentication authentication = (Authentication) principal;
+
+        ParticipantPrincipal participant = (ParticipantPrincipal) authentication.getPrincipal();
+
+        Long participantNo = participant.getParticipantNo();
+        Long roomNo = participant.getRoomNo();
+
 
         questionService.registerQuestion(dto.getRoomNo(),dto.getQuestion());
 
