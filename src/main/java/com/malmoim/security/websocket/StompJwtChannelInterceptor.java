3c75a9f21d98770b568dfa37bde79af7201a4900
domino3333 <busster@naver.com>
2026-07-28 11:54:53 +0900

    웹소켓에 대한 필터 생성 http의 jwt필터는 참여자 토큰을 못받으므로

---
+++ src/main/java/com/malmoim/security/websocket/StompJwtChannelInterceptor.java
@@ -0,0 +1,11 @@
+package com.malmoim.security.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StompJwtChannelInterceptor implements ChannelInterceptor {
+    
+}
