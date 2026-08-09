2f580aae36bd9c9beaf2a069cf6037516a7be988
domino3333 <busster@naver.com>
2026-08-09 23:22:50 +0900

    dto 파일이름 통일하기

--- src/main/java/com/malmoim/dto/qna/ParticipantListCountResponse.java
+++ src/main/java/com/malmoim/dto/qna/ParticipantListCountResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 @Builder
-public class ParticipantListCount {
+public class ParticipantListCountResponse {
     private Integer count;
     private List<Participant> list;
 }
