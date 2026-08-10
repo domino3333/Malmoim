c8fdfb715b6227a5104276ec5bf0f4eae318389e
domino3333 <busster@naver.com>
2026-08-10 19:39:04 +0900

    StompHeaderAccessor로 로그 테스트

--- src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
+++ src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
@@ -3,6 +3,7 @@ package com.malmoim.websocket.qna;
 import com.malmoim.security.ParticipantPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
@@ -30,8 +31,17 @@ public class QnaPresenceEventListener {
         }
 
         ParticipantPrincipal participantPrincipal =(ParticipantPrincipal) principal;
-
         System.out.println("연결된 참여자:" + participantPrincipal.getNickname());
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String sessionId = accessor.getSessionId();
+
+        if(sessionId == null){
+            return;
+        }
+
+        System.out.println("WebSocket sessionId: " + sessionId);
 
     }
 }
