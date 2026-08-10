026fc4580f23d1d68acf34f91223e58cf61335b0
domino3333 <busster@naver.com>
2026-08-10 18:10:38 +0900

    웹소켓 연결을 듣기 위한 리스너 handleConnected 메서드 생성

--- src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
+++ src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
@@ -1,4 +1,18 @@
 package com.malmoim.websocket.qna;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+@Component
+@RequiredArgsConstructor
 public class QnaPresenceEventListener {
+
+    private final QnaPresenceRegistry qnaPresenceRegistry;
+
+    @EventListener
+    public void handleConnected(SessionConnectedEvent event){
+
+    }
 }
