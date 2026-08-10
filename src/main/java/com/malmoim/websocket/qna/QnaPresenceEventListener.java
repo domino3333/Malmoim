44b420aa436ac15a4724d383abbc61f9d2b1f6c6
domino3333 <busster@naver.com>
2026-08-10 19:11:14 +0900

    인터셉터에서 넣었던 참여자 정보를 이벤트 리스너에서 꺼내는 코드 작성

--- src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
+++ src/main/java/com/malmoim/websocket/qna/QnaPresenceEventListener.java
@@ -1,9 +1,13 @@
 package com.malmoim.websocket.qna;
 
+import com.malmoim.security.ParticipantPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+import java.security.Principal;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +17,17 @@ public class QnaPresenceEventListener {
 
     @EventListener
     public void handleConnected(SessionConnectedEvent event){
+        Principal user = event.getUser();
+
+        if(!(user instanceof Authentication authentication)){
+            return;
+        }
+
+        if(!(authentication.getPrincipal() instanceof ParticipantPrincipal participantPrincipal)){
+            return;
+        }
+
+        System.out.println("연결된 참여자:" + participantPrincipal.getNickname());
 
     }
 }
