cf5e3a15297b1779bd12c2b502bf973bf47c0dde
domino3333 <busster@naver.com>
2026-08-23 06:13:47 +0900

    응답용 qna info dto 생성

---
+++ src/main/java/com/malmoim/dto/qna/QnaRoomInfoResponse.java
@@ -0,0 +1,19 @@
+package com.malmoim.dto.qna;
+
+import com.malmoim.domain.QnaPhase;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class QnaRoomInfoResponse {
+    private Long no;
+    private Long hostNo;
+    private String title;
+    private String code;
+    private Integer capacity;
+    private LocalDateTime createdAt;
+    private String type;
+    private String visibility;
+    private QnaPhase status;
+}
