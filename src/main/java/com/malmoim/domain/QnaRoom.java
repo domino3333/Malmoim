584b9ed854ee65436295b79dd0cd422d053e3361
domino3333 <busster@naver.com>
2026-08-27 01:46:18 +0900

    db 컬럼 추가에 따른 필드 추가 투표시작,종료 시간 추가

--- src/main/java/com/malmoim/domain/QnaRoom.java
+++ src/main/java/com/malmoim/domain/QnaRoom.java
@@ -13,6 +13,8 @@ public class QnaRoom {
     private Long roomNo;
     private LocalDateTime questionStartedAt;
     private LocalDateTime questionEndedAt;
+    private LocalDateTime votingStartedAt;
+    private LocalDateTime votingEndedAt;
     private QnaPhase status;
 
 }
