c1602967ed752b4ac8d24a60c7461b2758adc5cb
domino3333 <busster@naver.com>
2026-08-28 00:06:59 +0900

    dto에 유효성 검증 및 메시지 추가

--- src/main/java/com/malmoim/dto/qna/timer/StartQnaPhaseRequest.java
+++ src/main/java/com/malmoim/dto/qna/timer/StartQnaPhaseRequest.java
@@ -1,10 +1,16 @@
 package com.malmoim.dto.qna.timer;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class StartQnaPhaseRequest {
 
+    @NotNull(message = "시간을 반드시 입력")
+    @Positive(message = "시간은 1초 이상")
+    @Max(value = 3600, message = "시간은 최대 60분")
     private Long durationSeconds;
 }
