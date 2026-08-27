5cb597666aea84488728b0ff8d63462b7088c7a6
domino3333 <busster@naver.com>
2026-08-28 00:08:00 +0900

    주석 추가

--- src/main/java/com/malmoim/dto/qna/timer/StartQnaPhaseRequest.java
+++ src/main/java/com/malmoim/dto/qna/timer/StartQnaPhaseRequest.java
@@ -9,8 +9,9 @@ import lombok.Data;
 @Data
 public class StartQnaPhaseRequest {
 
-    @NotNull(message = "시간을 반드시 입력")
-    @Positive(message = "시간은 1초 이상")
+    //null, 0, 음수를 막기위함
+    @NotNull(message = "시간을 반드시 입력") // null인지 검사
+    @Positive(message = "시간은 1초 이상") //0보다 큰 값인지 검사
     @Max(value = 3600, message = "시간은 최대 60분")
     private Long durationSeconds;
 }
