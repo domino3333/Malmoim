a97592a301a5af6a6d5927eb69dfb1d91fb583f2
domino3333 <busster@naver.com>
2026-09-01 22:53:07 +0900

    정렬된 질문 리스트 http 스냅샷을 위한 dto 생성

---
+++ src/main/java/com/malmoim/dto/qna/phase/AnsweringResultResponse.java
@@ -0,0 +1,14 @@
+package com.malmoim.dto.qna.phase;
+
+import com.malmoim.dto.qna.vote.VoteResultResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class AnsweringResultResponse {
+    private QnaPhaseResponse qnaPhaseResponse;
+    private List<VoteResultResponse> voteResultResponseList;
+}
