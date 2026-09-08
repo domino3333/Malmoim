de24188844ea606f04b6a2ff30d65e48698eadf6
domino3333 <busster@naver.com>
2026-09-08 10:47:02 +0900

    코드 정리

--- src/main/java/com/malmoim/security/websocket/StompJwtChannelInterceptor.java
+++ src/main/java/com/malmoim/security/websocket/StompJwtChannelInterceptor.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.AccessDeniedException;
 import java.util.Objects;
 
 @Component
