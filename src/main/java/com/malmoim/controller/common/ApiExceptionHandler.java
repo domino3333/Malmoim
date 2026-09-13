25abb7a5e0a858b3b14f08de14f9232a06b1649f
domino3333 <busster@naver.com>
2026-09-13 12:16:52 +0900

    예외 핸들러 추가

--- src/main/java/com/malmoim/controller/common/ApiExceptionHandler.java
+++ src/main/java/com/malmoim/controller/common/ApiExceptionHandler.java
@@ -3,11 +3,27 @@ package com.malmoim.controller.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException exception) {
+        FieldError error = exception.getBindingResult().getFieldError();
+        String message = error != null && error.getDefaultMessage() != null
+                ? error.getDefaultMessage() : "입력값을 확인해주세요";
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatus(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode()).body(exception.getReason());
+    }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<String> handleAccessDenied(AccessDeniedException exception) {
