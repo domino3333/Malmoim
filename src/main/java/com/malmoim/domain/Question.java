fbcd598200c7e1aacefb1a8cf919ed3649b020c9
domino3333 <busster@naver.com>
2026-09-18 14:52:18 +0900

    매퍼가 컬럼의 순서를 몰라도 되도록 어노테이션 설정 및 에러 해결

--- src/main/java/com/malmoim/domain/Question.java
+++ src/main/java/com/malmoim/domain/Question.java
@@ -1,12 +1,16 @@
 package com.malmoim.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Question {
 
     private Long no; // question 테이블 PK
