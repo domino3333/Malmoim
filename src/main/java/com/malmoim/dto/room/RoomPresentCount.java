3919291327ebef4151146873d60b2e9a59358111
domino3333 <busster@naver.com>
2026-09-26 04:19:14 +0900

    현인원 수 dto 추가

---
+++ src/main/java/com/malmoim/dto/room/RoomPresentCount.java
@@ -0,0 +1,16 @@
+package com.malmoim.dto.room;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoomPresentCount {
+
+    private Integer presentCount;
+}
